b4c08075b6e4932960cdcad46dd8c1577c6d3228
alisher <shera.rashiduulu@gmail.com>
2022-06-08 15:38:19 +0600

    Создал файл req.txt и первый модель Наши преимущества

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Advantages(models.Model):
+    image = models.ImageField(upload_to='gallery_images', null=True, blank=True, verbose_name='Фото')
+    header = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.CharField(max_length=200, verbose_name='Описание')
+
+    def __str__(self):
+        return self.header
